ae217d1651262c66f5699d5b12269b89fee2c02e
Caique Piazzaroli <141646422+CaiquePiazzaroli@users.noreply.github.com>
2024-01-23 15:22:09 -0300

    Update script, adição de comentarios e organização do código

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,9 @@
+//criando variáveis e selecionando a imagem, o título e o botão.
+const myImage = document.querySelector("img");
+let myheading = document.querySelector("h1");
+let myButton = document.querySelector("button");
+
+//Função que altera o atributo da imagem no html
 function teste () {
     const mySrc = myImage.getAttribute("src");
     if (mySrc === "img/mascote_javascript.svg") {
@@ -6,7 +12,10 @@ function teste () {
       myImage.setAttribute("src", "img/mascote_javascript.svg");
     }
   }
-
+  
+// Função que chama um prompt para o ususario para digitar um nome,
+// vefica se um nome foi digitado e armazena no localstorage e
+// diciona o nome ao titulo no html.
 function setUserName() {
     const myName = prompt("Por favor digite o seu nome.");
     if (!myName) {
@@ -15,21 +24,20 @@ function setUserName() {
       localStorage.setItem("name", myName);
       myHeading.textContent = `Mozilla é legal, ${myName}`;
     }
+
 }
 
-const myImage = document.querySelector("img");
+//Verifica se existe ou nao um nome no localstorage, se exisstir, 
+// adiciona ao titulo, se nao, executa a função setUserName.
+if (!localStorage.getItem("name")){
+  setUserName();
+}else {
+   const storedName = localStorage.getItem("name");
+   myheading.textContent = `Javascript é legal, ${storedName}`;
+}
+
 myImage.onclick = teste;
-
-
-let myButton = document.querySelector("button");
 myButton.onclick = () => {
     setUserName();
-}
-
-let myheading = document.querySelector("h1")
-if (!localStorage.getItem("name")){
-    setUserName();
-}else {
-    const storedName = localStorage.getItem("name");
-    myheading.textContent = `Javascript é legal, ${storedName}`;
+    
 }
